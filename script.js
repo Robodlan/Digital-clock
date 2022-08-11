2c241952e04ee77b24f5b900b62a2711733f9cc1
Robodlan <Robodlan@users.noreply.github.com>
2022-08-11 12:14:15 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let clock = () =>{
    let mins = date.getMinutes();
    let secs = date.getSeconds();
    let period = "AM";
-   if (hrs == 0) {
+   if (hrs == 12) {
        hrs = 12;
    } else if (hrs >= 12) {
        hrs = hrs - 12;
        period = "PM"
    }
-//    hrs = hrs < 10 ? `0${hrs}` : hrs;
+   hrs = hrs < 10 ? `0${hrs}` : hrs;
    mins = mins < 10 ? `0${mins}` : mins;
    secs = secs < 10 ? `0${secs}` : secs;
    let time = `${hrs}:${mins}:${secs} ${period}`;
